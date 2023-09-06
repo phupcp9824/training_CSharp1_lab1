f1ab37a79be6fcc68bb6e8f314bca738dae85c03
User Name <Admin@DESKTOP-7KKDTTL>
2023-09-06 15:06:22 +0700

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class Studen
+    {
+        public static void printName(string firstname, string lastname)
+        {
+            string name = string.Format("{0} {1}", firstname, lastname);
+            Console.WriteLine("Studen name:"+name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Studen.printName("fpoly","cntt");
+            Studen.printName("tkdh", "ptpt");
+            Console.ReadKey();
+        }
+
+    }
+}
